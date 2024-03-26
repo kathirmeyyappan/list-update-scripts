21fa25d9c8e08b266a7b407ce124efd64accabcd
kathirmeyyappan <kathirmey@gmail.com>
2024-03-26 14:03:50 -0500

    update python script for sheet changes

--- python-scripts/update_mal_scores.py
+++ python-scripts/update_mal_scores.py
@@ -25,8 +25,10 @@ mal_client_id = os.getenv('MAL_CLIENT_ID')
 mal_auth = {'X-MAL-CLIENT-ID' : mal_client_id}
 
 # arrays for use, cmp, and update
-urls = [url for url in sheet.col_values(12)[1:]]
-old_scores = [float(n) for n in sheet.col_values(8)[1:]]
+urls = [url for url in sheet.col_values(13)[1:]]
+print(urls)
+old_scores = [0 if not n else float(n) for n in sheet.col_values(8)[1:]]
+print(old_scores)
 new_scores = [[]]
 anime_names = sheet.col_values(3)[1:]
 updates = []
